e70cb19761235c762adcb7f2a63f8a1e35f58282
Simon Brungs <00pflaume@gmail.com>
2017-01-21 21:09:19 +0100

    Fixed wrong config file

--- src/main/java/de/simonsator/partyandfriends/cfr/CFRMain.java
+++ src/main/java/de/simonsator/partyandfriends/cfr/CFRMain.java
@@ -16,7 +16,7 @@ public class CFRMain extends Plugin {
 	@Override
 	public void onEnable() {
 		try {
-			Configuration config = (new CFRConfig(new File(getDataFolder(), "config,yml"))).getCreatedConfiguration();
+			Configuration config = (new CFRConfig(new File(getDataFolder(), "config.yml"))).getCreatedConfiguration();
 			Friends.getInstance().addCommand(new CancelCommand(config.getStringList("Commands.CancelCommand.Name").toArray(new String[0]),
 					config.getInt("Commands.CancelCommand.Priority"), config.getString("Message.CommandUsage"), config));
 		} catch (IOException e) {
