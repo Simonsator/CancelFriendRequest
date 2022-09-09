b603252c6674eb22e4343b18d532747825ab8cf1
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-09-09 01:08:28 -0400

    Fix Run

--- src/main/java/de/simonsator/partyandfriends/velocity/cfr/CFRLoader.java
+++ src/main/java/de/simonsator/partyandfriends/velocity/cfr/CFRLoader.java
@@ -5,6 +5,7 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import de.simonsator.partyandfriends.velocity.VelocityExtensionLoadingInfo;
 import de.simonsator.partyandfriends.velocity.main.PAFPlugin;
 
@@ -14,21 +15,20 @@ import java.nio.file.Path;
 		url = "https://www.spigotmc.org/resources/cancel-friend-requests-for-party-and-friends.34567/",
 		description = "An add-on for party and friends to cancel friend requests"
 		, authors = {"JT122406", "Simonsator"}, dependencies = {@Dependency(id = "partyandfriends")})
-public class CFRLoader {
+	public class CFRLoader {
 
+		private final Path folder;
 
-	private final Path folder;
+		@Inject
+		public CFRLoader(@DataDirectory final Path pFolder) {
+			folder = pFolder;
+		}
 
-	@Inject
-	public CFRLoader(Path pFolder) {
-		folder = pFolder;
+		@Subscribe
+		public void onProxyInitialization(ProxyInitializeEvent event) {
+			PAFPlugin.loadExtension(new VelocityExtensionLoadingInfo(new CFRMain(folder),
+					"cancelfriendrequest",
+					"CancelFriendRequest",
+					"1.0.4-RELEASE", "JT122406, Simonsator"));
+		}
 	}
-
-	@Subscribe
-	public void onProxyInitialization(ProxyInitializeEvent event) {
-		PAFPlugin.loadExtension(new VelocityExtensionLoadingInfo(new CFRMain(folder),
-				"cancelfriendrequest",
-				"CancelFriendRequest",
-				"1.0.4-RELEASE", "JT122406, Simonsator"));
-	}
-}
