f190a54e5c21e36496dda209a444598f832cb6eb
Simon Brungs <00pflaume@gmail.com>
2026-07-12 15:51:04 +0200

    Use velocity-plugin.json

--- src/main/java/de/simonsator/partyandfriends/velocity/cfr/CFRLoader.java
+++ src/main/java/de/simonsator/partyandfriends/velocity/cfr/CFRLoader.java
@@ -3,18 +3,12 @@ package de.simonsator.partyandfriends.velocity.cfr;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
-import com.velocitypowered.api.plugin.Dependency;
-import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import de.simonsator.partyandfriends.velocity.VelocityExtensionLoadingInfo;
 import de.simonsator.partyandfriends.velocity.main.PAFPlugin;
 
 import java.nio.file.Path;
 
-@Plugin(id = "cancelfriendrequest", name = "CancelFriendRequest", version = "1.0.6-RELEASE",
-		url = "https://www.spigotmc.org/resources/cancel-friend-requests-for-party-and-friends.34567/",
-		description = "An add-on for party and friends to cancel friend requests"
-		, authors = {"JT122406", "Simonsator"}, dependencies = {@Dependency(id = "partyandfriends")})
 public class CFRLoader {
 
 	private final Path folder;
